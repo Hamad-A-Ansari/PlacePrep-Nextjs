2d23aeabe5538f4f858316346d99c1630522278d
Hamad Ahmad Ansari <hamad.ansarif90@gmail.com>
2025-06-21 22:15:43 +0530

    Temp(Feat: Add Home page)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
     <>
+      {/* NAVBAR */}
       <section className='card-cta'>
         <div className='flex flex-col gap-6 max-w-lg'>
           <h2>Get <span className='text-success-100'>Placement Ready</span> with AI Powered Mock Interview & Feedback</h2>
@@ -12,6 +14,23 @@ const page = () => {
           <Button asChild className='btn-primary max-sm:w-full'>
             <Link href="/interview">Start an Interview</Link>
           </Button>
+        </div>
+        <Image src="/robot.png" alt='robo-dude' width={400} height={400} className='max-sm:hidden' />
+      </section>
+
+      {/* PREV INTERVIEWS */}
+      <section className='flex flex-col gap-6 mt-8'>
+        <h2>Your Interviews</h2>
+        <div className='interviews-section'>
+          <p>You haven&apos;t taken any interviews yet</p>
+        </div>
+      </section>
+
+      {/* TAKE INTERVIEW */}
+      <section className='flex flex-col gap-6 mt-8'>
+        <h2>Take an Interview</h2>
+        <div className='interviews-section'>
+          <p>There are no interviews available right now.</p>
         </div>
       </section>
     </>
